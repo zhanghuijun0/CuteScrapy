9d663a55cc58dee57562b11a35adad816772750a
huijun zhang <zhanghuijun@souche.com>
2016-12-19 20:55:33 +0800

    oschina 改版

--- CuteScrapy/spiders/OSChinaSplider.py
+++ CuteScrapy/spiders/OSChinaSplider.py
@@ -1,5 +1,6 @@
 # coding:utf8
 import re
+import traceback
 from scrapy.cmdline import execute
 from scrapy import Request
 from scrapy.spiders import CrawlSpider
@@ -18,13 +19,12 @@ class OsChinaSplider(CrawlSpider):
 
     def start_requests(self):
         for item in self.kindList:
-            page = 1
-            id = item.get('id')
-            title = item.get('title')
-            url = 'http://www.oschina.net/blog?type=%s&p=%s'
+            classification = item.get('id')
+            label = item.get('title')
             yield Request(
-                url % (id, page),
-                meta={'type': 'list', 'page': page, 'id': id, 'title': title, 'url': 'http://www.oschina.net/blog'},
+                'https://www.oschina.net/action/ajax/get_more_recommend_blog?classification=%s&p=%s' % (
+                    classification, 1),
+                meta={'type': 'list', 'page_no': 1, 'classification': classification, 'label': label},
                 dont_filter=True,
                 headers={
                     'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"}
@@ -34,49 +34,47 @@ class OsChinaSplider(CrawlSpider):
         if response.meta['type'] == 'list':
             for item in self.parse_oschina(response):
                 yield item
-        else:
-            yield
 
     def parse_oschina(self, response):
-        contentlist = response.xpath('//*[@id="RecentBlogs"]/ul[1]/li')
-        self.logger.info(u'-----------[%s]第%s页,%d条数据-----------' % (
-            response.meta['title'], response.meta['page'], len(contentlist)))
-        for item in contentlist:
-            article_url = item.xpath('div/h3/a/@href').extract_first()
-            article_title = item.xpath('div/h3/a/text()').extract_first()
-            brief = item.xpath('div/p/text()').extract_first()
-            dateline = item.xpath('div/div/text()').extract_first()
-            blog_url = item.xpath('a/@href').extract_first()
-            nick_name = dateline.split(' ')[0]
-            dateline = dateline.split(' ')[2]
-            self.logger.info('title:' + article_title)
-            item = BlogsItem()
-            item['site'] = self.site
-            item['url'] = article_url
-            item['title'] = article_title
-            item['label'] = response.meta['title']
-            item['brief'] = brief.strip() if brief is not None else None
-            item['post_date'] = dateline
-            item['blog'] = blog_url
-            item['author'] = nick_name
-            item['pv'] = None
-            item['num_reviews'] = None
-            item['diggnum'] = None
-            item['burynum'] = None
-            yield item
-        # next_page = response.xpath('//*[@id="RecentBlogs"]/ul[@class="pager"]/li[@class="page next"]/a/@href').extract()
-        next_page = response.xpath('//li[@class="page next"]/a/@href').extract_first()
+        list = response.xpath('//body/div')
+        for sec in list:
+            try:
+                item = BlogsItem()
+                item['site'] = self.site
+                item['url'] = sec.xpath('div[@class="box-fl"]/a/@href').extract_first()
+                item['title'] = sec.xpath('div[@class="box-aw"]/header/a/@title').extract_first()
+                item['label'] = response.meta['label']
+                item['brief'] = sec.xpath('div[@class="box-aw"]/section/text()').extract_first()
+                item['post_date'] = sec.xpath('div[@class="box-aw"]/footer/span/text()').extract()[2]
+                item['blog'] = sec.xpath('div[@class="box-aw"]/header/a/@href').extract_first()
+                item['author'] = sec.xpath('div[@class="box-fl"]/a/img/@alt').extract_first()
+                footer = sec.xpath('div[@class="box-aw"]/footer').extract_first()
+                pv = re.search(u'阅读 (\d+)', footer)
+                num_reviews = re.search(u'评论 (\d+)', footer)
+                diggnum = re.search(u'点赞 (\d+)', footer)
+                item['pv'] = pv.group(1) if pv else None
+                item['num_reviews'] = num_reviews.group(1) if num_reviews else None
+                item['diggnum'] = diggnum.group(1) if diggnum else None
+                item['burynum'] = None
+                self.logger.info('title:%s' % item['title'])
+                yield item
+            except Exception as e:
+                traceback.print_exc()
+                self.logger.error(e)
+        next_page = response.xpath('//body/a/@href').extract_first()
         if next_page:
-            page = re.search('p=(\d+)', next_page).group(1)
-            url = response.meta['url']
+            next_page_no = re.search('p=(\d+)', next_page).group(1)
             yield Request(
-                url + next_page,
-                meta={'type': 'list', 'page': page, 'id': response.meta['id'], 'title': response.meta['title'],
-                      'url': url},
+                'https://www.oschina.net%s' % (next_page),
+                meta={'type': 'list', 'page_no': next_page_no, 'classification': response.meta['classification'],
+                      'label': response.meta['label']},
                 dont_filter=True,
                 headers={
                     'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"}
             )
+        else:
+            self.logger.info(
+                'page_no:%s,msg:%s' % (response.meta['page_no'], response.xpath('//body/p/text()').extract_first()))
 
 
 if __name__ == '__main__':
